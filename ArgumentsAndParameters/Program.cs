eaf55121167ee0b1ddbb17437c2edc78554e3a30
susslik1 <saskia.suurmets@tktk.ee>
2024-12-03 16:44:03 +0200

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+
+PrintAnyWord(userInput); // userImput- argument
+
+
+static void PrintAnyWord(string anyString) // anystring - parameeter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine (anyString);
+    }
+  
+}
